3025ef271f535d8aba6d2799f5d9b1e947e70b28
Ranjit67 <sahooranjit519@gmail.com>
2021-02-07 22:50:35 +0530

    Delete file.

--- data.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-  JWT_SECURE:"HFSHGSHfhsdgyetmvbmnvbxngdhfksdkljfksdjgb xncb vbnxcgfgfhsbmnbxcmnxbmnxc",
-  MONGODATA:"mongodb+srv://ranjit_7:nOFEeg9r8GMaFwC0@cluster0.y5crp.mongodb.net/SongpaluDB?retryWrites=true&w=majority"
-}
-//nOFEeg9r8GMaFwC0
-//mongodb+srv://ranjit_7:nOFEeg9r8GMaFwC0@cluster0.y5crp.mongodb.net/SongpaluDB?retryWrites=true&w=majority
